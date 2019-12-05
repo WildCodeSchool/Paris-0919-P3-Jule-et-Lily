9b0a20cf060270171639f86ca5dbe29820a93ee2
Fanny <fanny.bouffier@gmail.com>
2019-12-05 16:43:24 +0100

    feat(route):add route for displaying orders statistics for current month order and orders by status

--- back/conf.js
+++ back/conf.js
@@ -4,6 +4,7 @@ console.log(process.env.DB_HOST);
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST, 
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password:process.env.DB_PASS,
     database: 'jule_et_lily_bdd', // le nom de la base de données
--- back/index.js
+++ back/index.js
@@ -78,7 +78,46 @@ app.put('/api/dashboard/product/image/:id', (req, res) => {
   });
 });
 
-//___________________________________________________________________________________________________
-/////////////////////////////////////// Autres requetes /////////////////////////////////////////////
+
+//__________________________________________________________________________________________________________________________________________
+/////////////////////////////////////// Get les orders depuis le début du mois / les orders en cours ou expédiées ////////////////////////////////////////
+
+
+  app.get("/api/dashboard/order/current-month", (req, res) => {
+    // connection à la base de données, et sélection des employés
+    connection.query(
+      `SELECT * FROM orders
+      WHERE MONTH(order_date) = MONTH(CURRENT_DATE)
+      AND YEAR(order_date) = YEAR(CURRENT_DATE)`,
+      (err, results) => {
+        if (err) {
+          res.status(500).send('Erreur lors de la récupération des commandes du mois');
+        } else {
+          res.json(results);
+        }
+      });
+  });
+
+  
+  app.get(`/api/dashboard/order/status/:number`, (req, res) => {
+    connection.query(
+    `SELECT o.order_ref, st.order_status_name
+    FROM orders AS o
+    JOIN order_status AS st 
+    ON st.order_status_id=o.order_status
+    WHERE st.order_status_id=${req.params.number}
+    ORDER BY o.order_ref ASC;`,
+    (err, results) => {
+      if (err) {
+        res.status(500).send('Erreur lors de la récupération des commandes en cours');
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  
+
+
 
 app.listen(port, (err) => console.log(`Server is listening on ${port}`))
