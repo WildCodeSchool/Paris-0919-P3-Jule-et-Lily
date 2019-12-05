4195b411e2a367fb07e1829439bb1b6494dd2625
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-12-05 16:12:41 +0100

    feat(server):add route for displaying orders statistics

--- back/conf.js
+++ back/conf.js
@@ -5,7 +5,8 @@ console.log(process.env.DB_HOST);
 const connection = mysql.createConnection({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER,
-    password:process.env.DB_PASS,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
     database: 'jule_et_lily_bdd', // le nom de la base de données
 });
 
