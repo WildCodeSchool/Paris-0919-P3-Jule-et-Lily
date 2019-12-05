fbff9e080eace8dd515a40532227c5515804ced0
Fanny <fanny.bouffier@gmail.com>
2019-12-05 17:01:50 +0100

    fix(route):clean conf.js by removing port

--- back/conf.js
+++ back/conf.js
@@ -4,11 +4,9 @@ console.log(process.env.DB_HOST);
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST, 
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password:process.env.DB_PASS,
     database: 'jule_et_lily_bdd', // le nom de la base de données
 });
-
 
 module.exports = connection;    
