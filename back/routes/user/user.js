5a2a12a3ad4fa1032f32c18ad247df78289bdbf1
Fanny <fanny.bouffier@gmail.com>
2019-12-06 18:25:15 +0100

    fix(server-2):fixed route for user

--- back/routes/user/user.js
+++ back/routes/user/user.js
@@ -6,6 +6,7 @@ router.get('/', (req, res) => {
   res.send("je suis sur la route /user ").status(200)
 })
 
+
 router.route('/:role')
   .get(function (req, res) {
     connection.query(`SELECT * FROM user WHERE user_role=${req.params.role} ORDER BY user_lastname, user_firstname ASC`, (err, results) => {
