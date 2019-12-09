717bc5b259cec5047e8e227c40e2410d49d9e575
JennyViannay <jenny.viannay75@gmail.com>
2019-12-09 15:27:16 +0100

    add react app from scratch, change server port to 4000, add nodemon and script to launch server

---
+++ front/README.md
@@ -0,0 +1,1 @@
+## Ceci est le readme du front 
---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      Hello ici le front Jule et Lily
+    </div>
+  );
+}
+
+export default App;
