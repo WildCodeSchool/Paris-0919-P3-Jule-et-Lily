b756a5164f40962ed7a5f98baa87276a9c8f36cf
Fanny <fanny.bouffier@gmail.com>
2019-12-13 14:55:32 +0100

    feat(bootstrap buttonsActions): creation of see, modify, delete and add buttons

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ButtonsActions  from  './components/admin/common/ButtonsActions'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
 
       <h1 className='col-md-12 col-md-offset-5'>Hello ici le front Jule et Lily</h1>
+      <ButtonsActions/>
     </div>
   );
 }
---
+++ front/src/components/admin/common/ButtonsActions.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./sb-admin-2.min.css";
+
+const ButtonsActions = () => {
+  return (
+    <>
+     
+      <div class="mt-4 mb-2">
+        <code>.btn-circle .btn-lg</code>
+      </div>
+      <a href="#" class="btn grey btn-lg">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="#" class="btn grey btn-lg">
+        <i class="fas fa-check"></i>
+      </a>
+      <a href="#" class="btn grey btn-lg">
+        <i class="fas fa-info-circle"></i>
+      </a>
+    </>
+  );
+};
+
+export default ButtonsActions;
