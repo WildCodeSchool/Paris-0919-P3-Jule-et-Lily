a8a5a00bd49e89934f1f8953d3299a445b2608f2
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-12-13 16:44:19 +0100

    feat(front) add bootstrap pagination component

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Pagination from './components/admin/common/Pagination';
 
 function App() {
   return (
     <div className="App">
 
       <h1 className='col-md-12 col-md-offset-5'>Hello ici le front Jule et Lily</h1>
+      <Pagination />
     </div>
   );
 }
