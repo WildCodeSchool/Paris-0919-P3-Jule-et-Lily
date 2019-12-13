ad4130c61687b302d53224a6bce95fd426d12877
Fanny <fanny.bouffier@gmail.com>
2019-12-13 17:53:48 +0100

    feat(iconsAction): creation of actions buttons

--- front/src/components/admin/common/ButtonsActions.js
+++ front/src/components/admin/common/ButtonsActions.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./sb-admin-2.min.css";
+import "../../../assets/css/admin/global.css";
+import "../../../assets/css/admin/ButtonsActions.css";
+import See from "../../../assets/icons/IconSee.svg";
+import Modify from "../../../assets/icons/IconModify.svg";
+import Delete from "../../../assets/icons/IconDelete.svg";
+
+
 
 const ButtonsActions = () => {
   return (
     <>
-     
-      <div class="mt-4 mb-2">
-        <code>.btn-circle .btn-lg</code>
-      </div>
-      <a href="#" class="btn grey btn-lg">
-        <i class="fab fa-facebook-f"></i>
+      <a href="#" className="btn bg-grey btn-lg btn-See">
+      <img className="btn-icon" src = {See} alt="icon"/>
       </a>
-      <a href="#" class="btn grey btn-lg">
-        <i class="fas fa-check"></i>
+      <a href="#" className="btn bg-grey btn-lg btn-Modify">
+      <img className="btn-icon" src = {Modify} alt="icon"/>
       </a>
-      <a href="#" class="btn grey btn-lg">
-        <i class="fas fa-info-circle"></i>
+      <a href="#" className="btn bg-grey btn-lg btn-Delete">
+      <img className="btn-icon" src = {Delete} alt="icon"/>
       </a>
     </>
   );
