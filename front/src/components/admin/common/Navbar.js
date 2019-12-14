95989cce69ad4f06da896038f902bcc2610b6be6
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-12-14 19:33:15 +0100

    feat(navbar): added icons

--- front/src/components/admin/common/Navbar.js
+++ front/src/components/admin/common/Navbar.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react'
 import IconJL from '../../../assets/icons/LogoJuleetLily.svg'
+import IconCollections from '../../../assets/icons/IconsCollections.svg'
+import IconDash from '../../../assets/icons/IconDashboard.svg'
+import IconHome from '../../../assets/icons/IconFrontHome.svg'
+import IconOrders from '../../../assets/icons/IconOrders.svg'
+import IconProducts from '../../../assets/icons/IconProducts.svg'
+import IconPromos from '../../../assets/icons/IconPromos.svg'
+import IconUsers from '../../../assets/icons/IconUsers.svg'
+// import { Link } from 'react-router-dom'   !!!! replace a by Link after !!!!
 import './sb-admin-2.css'
 import './sb-admin-2.min.css'
 import './Navbar.css'
@@ -7,60 +15,65 @@ import './Navbar.css'
 const Navbar = () => {
         return (
             <div>
-                <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-                    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                        <div class="sidebar-brand-icon rotate-n-15">
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center">
                             <img className='logoJuleLily' src={IconJL} alt='Jule + Lily' />
-                        </div>
                     </a>
 
-                    <hr class="sidebar-divider" />
+                    <li className="nav-item active">
+                        <a className="nav-link">
+                            <img className='icons' src={IconDash} alt='' />
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
 
-                    <div class="sidebar-heading">Boutique</div>
-                    <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                            <i class="fas fa-fw fa-cog"></i>
+                    <hr className="sidebar-divider" />
+
+                    <div className="sidebar-heading">Boutique</div>
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                        <img className='icons' src={IconOrders} alt='' />
                             <span>Commandes</span>
                         </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                            <i class="fas fa-fw fa-wrench"></i>
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                        <img className='icons' src={IconUsers} alt='' />
                             <span>Clients</span>
                         </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                            <i class="fas fa-fw fa-wrench"></i>
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                        <img className='icons' src={IconProducts} alt='' />
                             <span>Produits</span>
                         </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                            <i class="fas fa-fw fa-wrench"></i>
+                    <li className="nav-item">
+                        <a class="nav-link collapsed" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                        <img className='icons' src={IconCollections} alt='' />
                             <span>Collections + catégories</span>
                         </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-                            <i class="fas fa-fw fa-wrench"></i>
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+                        <img className='icons' src={IconPromos} alt='' />
                             <span>Promos + codes promo</span>
                         </a>
                     </li>
 
-                    <hr class="sidebar-divider" />
+                    <hr className="sidebar-divider" />
 
-                    <div class="sidebar-heading">Site</div>
+                    <div className="sidebar-heading">Site</div>
 
-                    <li class="nav-item">
-                        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-                            <i class="fas fa-fw fa-folder"></i>
-                                <span>Page d'accueil</span>
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+                            <img className='icons' src={IconHome} alt='' />
+                            <span>Page d'accueil</span>
                         </a>
                     </li>
                 </ul>
