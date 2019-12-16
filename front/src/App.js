2bd81d3aaee1831a03a582108156de7026ae127e
Fanny <fanny.bouffier@gmail.com>
2019-12-16 16:27:14 +0100

    feat (iconsActions): separation of each button in one distinct component

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ButtonsActions  from  './components/admin/common/ButtonsActions'
+import ButtonsAdd  from  './components/admin/common/ButtonsAdd'
+import ButtonsModify  from  './components/admin/common/ButtonsModify'
+import ButtonsSee  from  './components/admin/common/ButtonsSee'
+import ButtonsDelete  from  './components/admin/common/ButtonsDelete'
+
 
 const App = () => {
   return (
     <div className="App">
 
       <h1 className='col-md-12 col-md-offset-5'>Hello ici le front Jule et Lily</h1>
-      <ButtonsActions/>
+      <ButtonsAdd/>
+      <ButtonsModify/>
+      <ButtonsSee/>
+      <ButtonsDelete/>
     </div>
   );
 }
---
+++ front/src/components/admin/common/ButtonModify.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import "../../../assets/css/sb-admin-2.min.css";
+import "../../../assets/css/admin/global.css";
+import "../../../assets/css/admin/buttons-actions.css";
+
+
+
+const ButtonModify = () => {
+  return (
+    <>
+  
+    {/* Button Modify */}
+    <div>
+    <a href="#" className="btn bg-gray btn-sm btn-action">
+      <i class="fas fa-pen"></i>
+      </a>
+    </div>
+      
+    </>
+  );
+};
+
+export default ButtonModify;
