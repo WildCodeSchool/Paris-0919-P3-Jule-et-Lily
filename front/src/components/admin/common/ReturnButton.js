ee19610e2e9613c4688149c2a643d1fef53078fd
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-12-16 11:31:33 +0100

    feat(front) change colo to gray

--- front/src/components/admin/common/ReturnButton.js
+++ front/src/components/admin/common/ReturnButton.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
+import '../../../assets/css/admin/global.css'
 
  const ReturnButton = (props) => {
     return (
         <div>
-            <button type="button" className="btn btn-link .lightgray">&laquo; retour {props.returnPage}</button>
+            <button type="button" className="btn btn-link gray">&laquo; retour {props.returnPage}</button>
         </div>
     )
 }
