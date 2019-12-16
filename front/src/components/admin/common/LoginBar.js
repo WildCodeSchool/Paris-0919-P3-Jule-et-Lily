57721b4da1306562c539e15f4a6be74a5d3a9459
Gaetan <gaetan@protonmail.com>
2019-12-16 14:03:30 +0100

    feat(loginBar) add the good profil logo and color

--- front/src/components/admin/common/LoginBar.js
+++ front/src/components/admin/common/LoginBar.js
@@ -7,30 +7,32 @@ import '../../../assets/css/admin/LoginBar.css'
 const LoginBar = () => {
 
   return (
-    <nav className='nav'>
-
-      <li className="list-group-item dropdown no-arrow border-0 right gray">
-
-        <button type="button" class="btn btn-mg btn-link-light border-right-5 align-baseline ">Se deconnecter  </button>
-        <span className=' text-decoration-none text-muted mr-2 align-baseline '> | Admin</span>
-        <a className="list-item dropdown-toggle" href="" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-          <img className="img-profile rounded-circle border-left" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
-        </a>
-        <div className="dropdown-menu dropdown-menu-right " aria-labelledby="userDropdown">
-          <a className="dropdown-item" href="">
-            <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-            Profil
-    </a>
-          <a className="dropdown-item" href="">
-            <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-            Paramètres du profil
-    </a>
 
 
-        </div>
+    <li className="list-group-item dropdown no-arrow border-0 right gray ">
 
-      </li>
-    </nav>
+      <button type="button" class="btn btn-mg btn-link-light border-right-5 align-baseline justify-content-end ">Se deconnecter  </button>
+      <span className=' text-decoration-none text-muted mr-2 align-baseline '> | Admin</span>
+      <a className="list-item dropdown-toggle" href="" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+        <i class="fas fa-user-circle gray"></i>
+      </a>
+      <div className="dropdown-menu dropdown-menu-right " aria-labelledby="userDropdown">
+        <a className="dropdown-item" href="">
+          <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+          Profil
+</a>
+        <a className="dropdown-item" href="">
+          <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+          Paramètres du profil
+</a>
+
+
+      </div>
+
+    </li>
+
+
+
 
 
   )
