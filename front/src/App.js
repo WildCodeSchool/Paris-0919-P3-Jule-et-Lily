31a03f618bc8dd731fc12d2a3438afde29419fd6
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2019-12-16 15:04:46 +0100

    feat(front) create the template of the application in App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../src/assets/css/admin/sb-admin-2.min.css'
 
 function App() {
   return (
-    <div className="App">
-
-      <h1 className='col-md-12 col-md-offset-5'>Hello ici le front Jule et Lily</h1>
+    <div id="wrapper">
+        {/* mettre la navbar/sidebar ici */}
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+         {/* Mettre la login bar ici  */}
+        <div id="container-fluid">
+        <h1 className="col-md-12 col-md-offset-5">
+          Hello ici le front Jule et Lily
+        </h1>
+          {/* Mettre les routes vers les autres composants ici  */}
+        </div>  
+      </div>
     </div>
+  </div>
+
   );
 }
 
