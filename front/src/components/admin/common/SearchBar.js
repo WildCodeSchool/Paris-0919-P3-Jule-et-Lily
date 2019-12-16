325d78a1810f57286149beb94c3d11a3a1918ea5
Venesto <steven.pigere@gmail.com>
2019-12-16 12:03:46 +0100

    feat(bootstrap) fix class to className

--- front/src/components/admin/common/SearchBar.js
+++ front/src/components/admin/common/SearchBar.js
@@ -3,11 +3,11 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <div>
-      <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+      <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
       <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"/>
-      <div class="input-group-append">
-        <button class="btn btn-primary" type="button">
-          <i class="fas fa-search fa-sm"></i>
+      <div className="input-group-append">
+        <button className="btn btn-primary" type="button">
+          <i className="fas fa-search fa-sm"></i>
         </button>
       </div>
       </form>
