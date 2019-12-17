c809eb9cdc2fbb62728ebd37340203dfcd260313
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-12-17 14:53:13 +0100

    feat(searchBar): added css

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
+import React from 'react'
 import SearchBar from './components/admin/common/SearchBar'
 
 function App() {
   return (
     <div className="App">
-
       <SearchBar />
     </div>
   );
