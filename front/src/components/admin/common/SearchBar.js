bd8919e90601bc11c85939568edf4e896b33d283
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-12-17 11:52:20 +0100

    feat(searchBar): added border on searchBar and delete the button

--- front/src/components/admin/common/SearchBar.js
+++ front/src/components/admin/common/SearchBar.js
@@ -1,15 +1,14 @@
 import React from 'react'
+
+import '../../../assets/css/admin/sb-admin-2.css'
+import '../../../assets/css/admin/sb-admin-2.min.css'
 import '../../../assets/css/admin/global.css'
 
 const SearchBar = () => {
   return (
     <div>
       <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-      <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"/>
-      <div className="input-group-append">
-        <button className="btn btn-primary" type="button">Valider
-        </button>
-      </div>
+        <input type="text" className="form-control bg-light small border-dark mt-2" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"/>
       </form>
     </div>
   )
