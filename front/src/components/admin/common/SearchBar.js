aea7924583e278083ec7c23a9d3d2b65214aead9
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2019-12-17 11:32:55 +0100

    feat(searchBar): added value at button

--- front/src/components/admin/common/SearchBar.js
+++ front/src/components/admin/common/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../../assets/css/admin/global.css'
 
 const SearchBar = () => {
   return (
@@ -6,8 +7,7 @@ const SearchBar = () => {
       <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
       <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"/>
       <div className="input-group-append">
-        <button className="btn btn-primary" type="button">
-          <i className="fas fa-search fa-sm"></i>
+        <button className="btn btn-primary" type="button">Valider
         </button>
       </div>
       </form>
