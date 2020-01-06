57a68629e048cbf523864809cd2a9892b82f29b9
Charlotte LECONTE <charlotte.leconte91@gmail.com>
2020-01-06 11:13:19 +0100

    feat(login): creation of login page and link to button deconnecter

---
+++ front/src/components/admin/pages/Login.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import { Form } from "../common/";
+
+export default function Login() {
+    return (
+        <div className='m-5'>
+            <h1>Connecte-toi</h1>
+            <div>
+                <Form />
+            </div>
+        </div>
+    )
+}
