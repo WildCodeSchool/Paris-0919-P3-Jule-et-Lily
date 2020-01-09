edae810339f2cca79eaf82f12e9ba33786a263c2
Fanny <fanny.bouffier@gmail.com>
2020-01-09 09:34:12 +0100

    removed reactCSS import from FormColorCustom.js

--- front/src/components/admin/pages/frontcustom/FormColorCustom.js
+++ front/src/components/admin/pages/frontcustom/FormColorCustom.js
@@ -1,46 +1,9 @@
 import React from "react";
-import reactCSS from 'reactcss'
 import { ChromePicker } from 'react-color'
 
 
 
 const FormColorCustom = (props) => {
-
-  // const styles = reactCSS({
-    // 'default': {
-    //   colorTitle: {
-    //     width: '36px',
-    //     height: '14px',
-    //     borderRadius: '2px',
-    //     backgroundColor: `${props.titleColor}`,
-    //   },
-      // colorBackground: {
-      //   width: '36px',
-      //   height: '14px',
-      //   borderRadius: '2px',
-      //   backgroundColor: `${props.backgroundColor}`,
-      // },
-      // swatch: {
-      //   padding: '5px',
-      //   backgroundColor: '#fff',
-      //   borderRadius: '1px',
-      //   boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-      //   display: 'inline-block',
-      //   cursor: 'pointer',
-      // },
-      // popover: {
-      //   position: 'absolute',
-      //   zIndex: '2',
-      // },
-      // cover: {
-      //   position: 'fixed',
-      //   top: '0px',
-      //   right: '0px',
-      //   bottom: '0px',
-      //   left: '0px',
-      // },
-  //   },
-  // });
 
   return (
     <div className="d-flex EncartForm" >
