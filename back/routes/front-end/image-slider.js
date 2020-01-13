fe935fbe2f6ae6bd970febb64d46e503bdb0ebed
Fanny <fanny.bouffier@gmail.com>
2020-01-13 15:31:43 +0100

    feat(upload): added feature to upload and delete images

--- back/routes/front-end/image-slider.js
+++ back/routes/front-end/image-slider.js
@@ -25,7 +25,7 @@ router.post("/", upload.array("file"), (req, res, next) => {
         const objectFile = {
           image_name : "public/" + file.originalname,
           is_slider_image : 1,
-          image_url : "http://localhost:4000/public/" + file.originalname,
+          image_url : "public/" + file.originalname,
         }
         console.log('objectFile', objectFile)
         console.log('path', file.path)
@@ -41,8 +41,9 @@ router.post("/", upload.array("file"), (req, res, next) => {
 });
 
 
-router.route(['/all', '/', ':id' ])
 
+router.route(['/all', '/', '/:id'])
+//////// AFFICHER TOUTES LES IMAGES
     .get(function (req, res, next) {   //http://localhost:3000/image-slider/all
         connection.query(`SELECT * FROM image WHERE is_slider_image = '1' ORDER BY image_id ASC`, (err, results) => {
           if (err) {
@@ -53,67 +54,68 @@ router.route(['/all', '/', ':id' ])
         })
       })
 
-      // .post(function (req, res) {   //http://localhost:3000/image-slider/
-      //   const formData = req.body;
-      //   connection.query('INSERT INTO image SET ?', formData, (err, results) => {
-      //     if (err) {
-      //       res.status(500).send("Erreur lors de l'ajout d'une image dans le slider");
-      //     } else {
-      //       res.sendStatus(200);
-      //     }
-      //   })
-      // })
 
-      .put(function (req, res) {  // http://localhost:3000/image-slider/4
-        const formData = req.body;
-        connection.query(`UPDATE image SET ? WHERE image_id=${req.params.id}`, formData, (err, results) => {
-          if (err) {
-            res.status(500).send("Erreur lors de la modification d'une image dans le slider");
-          } else {
-            res.sendStatus(200);
-          }
-        })
-      })
+// ////// SUPPRIMER UNE IMAGE SUR BDD
+// .delete(function (req, res) {       
+//   connection.query(`DELETE FROM image WHERE image_id=${req.params.id}`, (err, result) => {
+//     if (err) {
+//       res.status(500).send("Erreur lors de la suppression d'une image");
+//     } else {
+//       res.sendStatus(200);
+//     }
+//   });
+// })
 
-      .delete(function (req, res) {    //  http://localhost:3000/image-slider/5
-        connection.query(`DELETE FROM image WHERE image_id=${req.params.id}`, (err, results) => {
-          if (err) {
-            res.status(500).send("Erreur lors de la suppression d'une image");
-          } else {
-            res.sendStatus(200);
-          }
-        })
-      })
+//////// SUPPRIMER IMAGE SUR SERVER 
+.delete(function (req, res) { 
+    const path = res[0].image_name;
+    const path2 = JSON.stringify(path)
 
+    console.log("image name", path)
+    console.log("image name2", path2)
 
+  try {
+    fs.unlinkSync(path)
+    res.send("image bien supprimée").status(200);
+    //file removed
+  } catch(err) {
+    console.error(err)
+  }
+}
 
+////// SUPPRIMER UNE IMAGE BDD ET SERVEUR
+// .delete(function (req, res) { 
 
+//       connection.query(`SELECT image_name FROM image WHERE image_id = ${req.params.id}`, (err, result) => {
+//         if (err) {
+//           res.send("Erreur lors de la recuperation du nom d'image").status(500)
+//         } else {
+//           const path = res[0].image_name;
+//           const path2 = JSON.stringify(path)
 
+//           console.log("image name", path)
+//           console.log("image name2", path2)
 
+//           fs.unlink(path2, (err) => {
+//             if (err) {
+//               res.send("Erreur lors de la suppression d'une image sur le serveur").status(500)
+//               return
+
+//             } else {
+//               connection.query(`DELETE FROM image WHERE image_id=${req.params.id}`, (err, result) => {
+//                 if (err) {
+//                   res.status(500).send("Erreur lors de la suppression d'une image");
+//                 } else {
+//                   res.sendStatus(200);
+//                 }
+//               });
+//             }  
+//           }) 
+//         }
+//     })
+// });  
+   
 
       module.exports = router
 
 
-
-
-
-// ///////// Route JENNY
-// router.post("/file", upload.array("file"), (req, res, next) => {
-//   req.files.map(file => {
-//     fs.rename(file.path, "public/" + file.originalname, err => {
-//       if (err) {
-//         return res.send("Problem during travel").status(500);
-//       } else {
-//         const objectFile = {
-//           name : "public/" + file.originalname
-//         }
-//         connection.query("INSERT INTO file SET ?", objectFile, err => {
-//           if (err) {
-//             return res.send("Error ocurred").status(500);
-//           }
-//         })
-//       }
-//     })
-//   })
-//   return res.send("Files uploaded sucessfully").status(200);
-// });
