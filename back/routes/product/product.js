7caf0e46002dbecef308a1a20a26abfe772ac005
Gaetan <gaetan@protonmail.com>
2020-01-15 18:31:21 +0100

    add a new routes for creation product

--- back/routes/product/product.js
+++ back/routes/product/product.js
@@ -10,7 +10,19 @@ const router = express.Router()
 router.get('/', (req, res) => {
   res.send("je suis sur la route /product").status(200)
 })
-router.route(['/stock/:id'])
+
+router.route(['/productstockid']) 
+  .get(function (req, res) {
+    connection.query('SELECT product_id from product ORDER BY PRODUCT_ID DESC LIMIT 1', (err, results) => {
+      if (err) {
+        res.status(500).send("Erreur lors de la récupération de l'id du produit");
+      } else {
+        res.json(results).status(200);
+      }
+    })
+  })
+
+router.route(['/stock/:id', '/stock/:stock_quantity/and/:stock_min'])
   .get(function (req, res) { //récup un produit
     connection.query('SELECT * from stock WHERE stock_product_id = ?', req.params.id, (err, results) => {
       if (err) {
@@ -36,6 +48,33 @@ router.route(['/stock/:id'])
       }
     });
   })
+  .post(function (req, res) {
+    const stock_min = req.params.stock_min
+    const stock_quantity = req.params.stock_quantity
+    connection.query('SELECT  product_id FROM product ORDER BY product_id DESC LIMIT 1', (err, results) => {
+      if (err) {
+        res.status(500).send("Erreur lors de la récupération de l'id du produit");
+      } else {
+        const recuperationIdProduct = results[0].product_id
+        console.log('recuperationIdProduct', recuperationIdProduct[0]);
+        const newStock = {
+          stock_product_id: recuperationIdProduct,
+          stock_min: parseInt(stock_min),
+          stock_quantity: parseInt(stock_quantity)
+        }
+        console.log('newStock', newStock);
+        connection.query('INSERT INTO stock SET ?', newStock, (err, results) => {
+          if (err) {
+            res.status(500).send("Erreur lors de l'ajout du NewStock");
+          } else {
+            res.status(200);
+          }
+        });
+        res.status(200);
+      }
+    });
+  })
+
 router.route(['/all'])
   .get(function (req, res) { //récup un produit
     connection.query('SELECT p.*, s.stock_quantity as product_stock, c.collection_name, k.category_name FROM product as p LEFT OUTER JOIN stock as s ON s.stock_product_id = p.product_id LEFT OUTER JOIN collection as c on c.collection_id = p.product_collection_id LEFT OUTER JOIN category as k ON k.category_id = p.product_category_id', req.body, (err, results) => {
@@ -85,46 +124,47 @@ router.route(['/:id', '/'])
           }
         })
       }
-    })})
+    })
+  })
 
-      .put(function (req, res) { // modifier un produit
-        const requestProduct = req.params.id;
-        const formData = req.body;
-        connection.query('UPDATE product SET ? WHERE product_id=?', [formData, requestProduct], (err, results) => {
-          if (err) {
-            console.log('erreur back', err);
-            res.status(500).send("Erreur lors de la modification du produit");
-          } else {
-            console.log('res back', res);
-            console.log(results)
-            res.sendStatus(200);
-          }
-        });
-      })
-      .delete(function (req, res) { // supprimer un produit penser à supprimer dans la bdd la connection avec le stock id
-        connection.query(`DELETE FROM product WHERE product_id=${req.params.id}`, err => {
-          if (err) {
-            console.log(err);
-            res.send("Erreur lors de la suppression du produit").status(500);
-          } else {
-            res.sendStatus(200);
-          }
-        });
-      });
-
-    // image associée à un produit set à NULL 
-    router.put('/image/:id', (req, res) => {
-      const requestProduct = req.params.id;
-      const formData = req.body;
-      connection.query('UPDATE product SET product_image_id = 0 WHERE product_id = ?', [requestProduct], err => {
-        if (err) {
-          res.status(500).send("Erreur lors de la modification du produit" + err);
-        } else {
-          res.sendStatus(200);
-        }
-      });
+  .put(function (req, res) { // modifier un produit
+    const requestProduct = req.params.id;
+    const formData = req.body;
+    connection.query('UPDATE product SET ? WHERE product_id=?', [formData, requestProduct], (err, results) => {
+      if (err) {
+        console.log('erreur back', err);
+        res.status(500).send("Erreur lors de la modification du produit");
+      } else {
+        console.log('res back', res);
+        console.log(results)
+        res.sendStatus(200);
+      }
     });
+  })
+  .delete(function (req, res) { // supprimer un produit penser à supprimer dans la bdd la connection avec le stock id
+    connection.query(`DELETE FROM product WHERE product_id=${req.params.id}`, err => {
+      if (err) {
+        console.log(err);
+        res.send("Erreur lors de la suppression du produit").status(500);
+      } else {
+        res.sendStatus(200);
+      }
+    });
+  });
+
+// image associée à un produit set à NULL 
+router.put('/image/:id', (req, res) => {
+  const requestProduct = req.params.id;
+  const formData = req.body;
+  connection.query('UPDATE product SET product_image_id = 0 WHERE product_id = ?', [requestProduct], err => {
+    if (err) {
+      res.status(500).send("Erreur lors de la modification du produit" + err);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
 
 
 
-    module.exports = router
+module.exports = router
