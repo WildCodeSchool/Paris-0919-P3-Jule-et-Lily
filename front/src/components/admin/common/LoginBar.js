2186cf34dfdaf710234e0f9879085b53c6313371
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2020-01-21 10:38:38 +0100

    add comments

--- front/src/components/admin/common/LoginBar.js
+++ front/src/components/admin/common/LoginBar.js
@@ -14,11 +14,12 @@ const LoginBar = (props) => {
     )
   }
 
+  //hooks du login
   const [login, setLogin] = useState('admin');
 
 
   useEffect(() => {
-    fetch(`/profile/1`, {
+    fetch(`/profile/1`, { //on récupère les infos de l'admin
       headers: {
         Authorization: "Bearer " + props.token,
         method: 'POST'
@@ -27,7 +28,7 @@ const LoginBar = (props) => {
     })
       .then(res => res.json())
       .then(res => {
-        setLogin(res.user_login)
+        setLogin(res.user_login) 
       })
     },
   )
@@ -38,7 +39,7 @@ const LoginBar = (props) => {
       {props.children}
       <div className="loginItems">
       <button type="button" class="btn btn-mg btn-link-light border-right-5 align-baseline justify-content-end " onClick={logOut}>Se deconnecter</button>
-  <span className=' text-decoration-none text-muted mr-2 align-baseline '>|&nbsp;&nbsp;&nbsp;{login}</span>
+      <span className=' text-decoration-none text-muted mr-2 align-baseline '>|&nbsp;&nbsp;&nbsp;{login}</span> {/*on affiche le login de l'admin (&nbsp; => pour afficher plusieurs espaces) */}
       <a className="list-item dropdown-toggle" href="/#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
         <i className="fas fa-user-circle gray"></i>
       </a>
