0b22826002f4bc7b234999fb19e64459cc504393
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2020-01-21 10:28:39 +0100

    feat(loginBar) change to loginBar component to show user login instead of 'admin' and set link to profile page

--- front/src/components/admin/common/LoginBar.js
+++ front/src/components/admin/common/LoginBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import {Link} from 'react-router-dom'
 import { connect } from "react-redux";
 import '../../../assets/css/admin/global.css'
@@ -14,36 +14,47 @@ const LoginBar = (props) => {
     )
   }
 
-  return (
+  const [login, setLogin] = useState('admin');
 
+
+  useEffect(() => {
+    fetch(`/profile/1`, {
+      headers: {
+        Authorization: "Bearer " + props.token,
+        method: 'POST'
+      }
+      // body: JSON.stringify(props.user)
+    })
+      .then(res => res.json())
+      .then(res => {
+        setLogin(res.user_login)
+      })
+    },
+  )
+
+  return (
     <div className="dropdown no-arrow border-0 right gray loginBar"> 
       
       {props.children}
       <div className="loginItems">
       <button type="button" class="btn btn-mg btn-link-light border-right-5 align-baseline justify-content-end " onClick={logOut}>Se deconnecter</button>
-      <span className=' text-decoration-none text-muted mr-2 align-baseline '> | Admin</span>
+  <span className=' text-decoration-none text-muted mr-2 align-baseline '>|&nbsp;&nbsp;&nbsp;{login}</span>
       <a className="list-item dropdown-toggle" href="/#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
         <i className="fas fa-user-circle gray"></i>
       </a>
       <div className="dropdown-menu dropdown-menu-right " aria-labelledby="userDropdown">
-        <a className="dropdown-item" href="/#">
+        <a className="dropdown-item" href="/profile">
           <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-          Profil
-</a>
-        <a className="dropdown-item" href="/#">
-          <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-          Paramètres du profil
-</a>
-
-
-      </div>
+          Profile
+       </a>
+    </div>
       </div>
     </div>
-
-
-
-
   )
 }
+function mapStateToProps(state) {
+  return { token: state.auth.token };
+}
 
-export default LoginBar;
+export default connect(mapStateToProps)(LoginBar);
+
