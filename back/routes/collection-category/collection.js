446bf1d54fbb9146a0394e69c0503598bdbbdb2a
Paul-Arnaud Lacroix <paularnaudlacroix@gmail.com>
2020-01-23 17:24:52 +0100

    feat(upload) add upload functions to collection component

--- back/routes/collection-category/collection.js
+++ back/routes/collection-category/collection.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const connection = require('../../conf')
-
 const router = express.Router()
+const multer = require("multer");
+const upload = multer({ dest: "public/" });
+const fs = require("fs");
 
 router.get('/', (req, res) => {
   res.send("je suis sur la route /collection ").status(200)
@@ -61,5 +63,172 @@ router.route(['/:id','/'])
       }
     });
   });
+
+
+
+  /////////////// Récupérer les images de la  collection sauf l'image en cover ///////////
+/////////////////////////////////////////////////////////////////////////////////
+
+router
+.route(["/image/:id", "/image"])
+.get(function(req, res) {
+  //récup un produit
+  connection.query(
+    `SELECT image_id FROM image JOIN collection ON image_id = collection_cover_image_id WHERE collection_id= ${req.params.id}`,
+    (err1, res1) => {
+      if (err1) {
+        res
+          .status(500)
+          .send(
+            "Erreur lors de la récupération de l'image de couverture de la collections"
+          );
+        console.log("erreur recup image", err);
+      } else if (!res1[0]) {
+        connection.query(
+          `SELECT * FROM image WHERE image_collection_id = ${req.params.id} ORDER BY image_id ASC`,
+          (err, results) => {
+            if (err) {
+              res
+                .status(500)
+                .send(
+                  "Erreur lors de la récupération des images de la collection"
+                );
+            } else {
+              res.json(results);
+            }
+          }
+        );
+      } else {
+        const coverImageId = res1[0].image_id;
+        connection.query(
+          `SELECT * FROM image WHERE image_collection_id = ${req.params.id}  AND image_id != ${coverImageId} ORDER BY image_id ASC`,
+          (err, results) => {
+            if (err) {
+              res
+                .status(500)
+                .send(
+                  "Erreur lors de la récupération des images de la collection"
+                );
+              console.log("erreur  recup image", err);
+            } else {
+              res.json(results);
+            }
+          }
+        );
+      }
+    }
+  );
+})
+
+////// SUPPRIMER UNE IMAGE BDD ET SERVEUR
+.delete(function(req, res) {
+  connection.query(
+    `SELECT image_name FROM image WHERE image_id = ${req.params.id}`,
+    (err, result) => {
+      if (err) {
+        res
+          .send("Erreur lors de la recuperation du nom d'image")
+          .status(500);
+      } else {
+        const path = result[0].image_name;
+        console.log("image name", path);
+        try {
+          fs.unlinkSync(path);
+          connection.query(
+            `DELETE FROM image WHERE image_id=${req.params.id}`,
+            (err, result) => {
+              if (err) {
+                res
+                  .status(500)
+                  .send(
+                    "Erreur lors de la suppression d'une image dans la bdd"
+                  );
+              } else {
+                res.send("image bien supprimée").status(200);
+              }
+            }
+          );
+        } catch (err) {
+          res
+            .status(500)
+            .send(
+              "Erreur lors de la suppression d'une image dans le serveur public"
+            );
+          console.error(err);
+        }
+      }
+    }
+  );
+});
+
+// Gestion Image de couverture de la collection
+router.route(['/image-cover/:id', '/image-cover/:id/:collectionId'])
+.get(function (req, res) { //récup un produit
+connection.query(
+`SELECT * FROM image JOIN collection ON image_id = collection_cover_image_id WHERE collection_id= ${req.params.id}`,
+  (err, results) => {
+    if (err) {
+      res
+        .status(500)
+        .send("Erreur lors de la récupération de l'image de couverture de la collection");
+        console.log("erreur  recup image", err)
+    } else {
+      res.json(results);
+    }
+  }
+);
+}) 
+.put(function (req, res) { // modifier image produit
+const ImageId = req.params.id;
+const collectionId = req.params.collectionId;
+console.log('imageid',ImageId)
+console.log('collectionid',collectionId)
+connection.query(`UPDATE collection SET collection_cover_image_id=${ImageId} WHERE collection_id=${collectionId}`, (err, results) => {
+  if (err) {
+    console.log('erreur back', err);
+    res.status(500).send("Erreur lors de la modification de l'image de couverture de la collection");
+  } else {
+    console.log(results)
+    res.sendStatus(200);
+  }
+});
+})  
+
+///////// ajout image collection
+router.post("/image/:id", upload.array("file"), (req, res, next) => {
+let error =  false;
+
+console.log("file cote back",req.files)
+req.files.map(file => {
+
+let Timestamp = Math.round(new Date().getTime() / 1000)
+let FileName = file.originalname
+let regex1 = /\’\”\;\,\*\./gi;
+let NewFileName = FileName.replace(regex1,"").split(" ").join("").toLowerCase()
+
+fs.rename(file.path, `public/${Timestamp}${NewFileName}`, err => {
+  if (err) {
+    error=true;
+  } else {
+    const objectFile = {
+      image_name : `public/${Timestamp}${NewFileName}`,
+      is_slider_image : 0,
+      image_url : `public/${Timestamp}${NewFileName}`,
+      image_collection_id: `${req.params.id}` 
+    }
+    connection.query("INSERT INTO image SET ?", objectFile, err2 => {
+      if (err2) {
+        error=true;
+      } 
+    })
+  }
+})
+}) 
+if (error) {
+res.send("Problem when uploading files").status(500);
+} else
+return res.send("Files uploaded sucessfully").status(200);
+});
+
 
 module.exports = router
